b7eb632fe61a63d1e4dbcd319eb38e85be293a4e
usui den <C0A2302260@edu.teu.ac.jp>
2024-04-16 17:25:47 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,21 +13,34 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_image = pg.image.load("fig/3.png")
     kk_image = pg.transform.flip(kk_image, True, False)
+    kk_rect = kk_image.get_rect()
+    kk_rect.center = 300, 200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_list = pg.key.get_pressed()
+
+        if key_list[pg.K_UP]:
+            kk_rect.move_ip(0, -1)
+        if key_list[pg.K_DOWN]:
+            kk_rect.move_ip(0, 1)
+        if key_list[pg.K_RIGHT]:
+            kk_rect.move_ip(1, 0)
+        if key_list[pg.K_LEFT]:
+            kk_rect.move_ip(-1, 0)
+
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
 
-        screen.blit(kk_image, [300, 200])
+        screen.blit(kk_image, kk_rect)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-
 
 if __name__ == "__main__":
     pg.init()
